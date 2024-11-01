a15e0e7c2aa20509968a06a3a7071b7f1a44a59b
Tommi Laukkanen <tlaukkanen@users.noreply.github.com>
2024-11-01 21:35:01 +0200

    Add SYSTEM_ACCESSTOKEN environment variable and update task configuration for Node.js

--- buildandreleasetask/tests/success.ts
+++ buildandreleasetask/tests/success.ts
@@ -5,6 +5,7 @@ import path = require('path');
 let taskPath = path.join(__dirname, '..', 'index.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
+tmr.setInput('SystemVssConnection', 'token');
 tmr.setInput('comment', 'Test');
 tmr.setInput('active', 'true');
 tmr.run();
